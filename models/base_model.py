42caecbac7c2403ebd643fb08831edfb74d650ab
adebayo <adebayoalliy@gmail.com>
2024-05-19 17:34:37 -0700

    file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'updated_at' or key == 'created_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.strptime(kwargs[key], '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
         else:
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 import json
-""" 
-This module defines FileStorage that serializes instances to a JSON file and deserializes JSON file to instances.
+import os
+"""
+This module defines FileStorage that
+serializes instances to a JSON file
+and deserializes JSON file to instances.
 """
 
 
 class FileStorage:
-    """ main class that defines instances attributes """
-    
-    __file_path = "../file.json"
+    """
+    main class that defines instances
+    attributes
+    """
+
+    __file_path = "file.json"
     __objects = {}
 
     def __init__(self):
+        pass
         """ constructor function """
 
     def all(self):
@@ -22,11 +29,22 @@ class FileStorage:
         """ sets in __objects the obj with key <obj class name>.id """
         name = obj.__class__.__name__ + '.' + obj.id
         self.__objects[name] = obj
+
     def save(self):
         """ serializes __objects to the JSON file """
-        self.__file_path = json.dumps(self.__objects)
+        dic = {}
+        for key, value in self.__objects.items():
+            dic[key] = value.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(dic, f)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
-        if self.__file_path:
-            __objects = json.loads(self.__file_path)
+
+        from models.base_model import BaseModel
+        if os.path.exists(self.__file_path):
+            dic = {}
+            with open(self.__file_path, 'r') as f:
+                dic = json.load(f)
+            for key, value in dic.items():
+                self.__objects[key] = BaseModel(**value)
