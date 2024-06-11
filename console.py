8f8e9daf787ae48f491adbaceab57d78ffe8c56e
adebayo <adebayoalliy@gmail.com>
2024-06-11 06:00:57 -0700

    console

--- console.py
+++ console.py
@@ -6,5 +6,26 @@ of the command interpreter
 
 import cmd
 
-class HBNBCommand(cmd.Cmd):
 
+class HBNBCommand(cmd.Cmd):
+    """ Entry point """
+
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """ Quit command to exit the program """
+        return True
+
+    def do_quit(self, line):
+        """ Quit command to exit the program """
+        return True
+
+    def postloop(slef):
+        print
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
