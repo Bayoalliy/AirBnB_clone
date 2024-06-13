e4d7c5ae55d3211f95f1ec720f0da47b234fe571
adebayo <adebayoalliy@gmail.com>
2024-06-13 14:19:35 -0700

    repaired

--- models/place.py
+++ models/place.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """ Place class """
+    
     name = ""
     city_id = ""
     user_id = ""
@@ -13,6 +14,6 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    latitude = 0
-    longitude = 0
-    amenity_ids = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
