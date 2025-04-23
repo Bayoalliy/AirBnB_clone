5ba5ea57e9bf8d6a5bcfa12487fa9eef425d5865
Bayo <adebayoalliy@gmail.com>
2025-04-23 15:44:22 -0700

    ff

--- console.py
+++ console.py
@@ -30,11 +30,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """ Quit command to exit the program """
+        """ Quit command to exit the command interpreter """
         return True
 
-    def do_EOF(self, arg):
-        """exits command line"""
+    def do_EOF(self, line):
+        """ EOF command to exit the command interpreter """
         return True
 
     def emptyline(self):
