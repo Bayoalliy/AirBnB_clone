ab5140962bfc0a92b8e53380578f54b44edb4779
Bayo <adebayoalliy@gmail.com>
2025-04-26 14:25:36 -0700

    place

--- models/place.py
+++ models/place.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
 """
-Write a class Review that inherits from BaseModel:
+Write a class place that inherits from BaseModel:
 
-models/review.py
+models/place.py
 Public class attributes:
-place_id: string - empty string: it will be the Place.id
+city_id: string - empty string: it will be the City.id
 user_id: string - empty string: it will be the User.id
-text: string - empty string
+name: string - empty string
+description: string - empty string
+number_rooms: integer - 0
+number_bathrooms: integer - 0
+max_guest: integer - 0
+price_by_night: integer - 0
+latitude: float - 0.0
+longitude: float - 0.0
+amenity_ids: list of string - empty list: it will be the list of Amenity.id later
 
 Update FileStorage to manage correctly serialization and
-deserialization of Review.
+deserialization of Place.
 
 Update your command interpreter (console.py) to allow show,
-create, destroy, update and all used with Review.
+create, destroy, update and all used with Place.
 """
 from models.base_model import BaseModel
 from models import storage
 
 
-class Review(BaseModel):
-    """inherits from BaseModel and defines the review object"""
-    place_id = ""
+class Place(BaseModel):
+    """inherits from BaseModel and defines the place object"""
+    city_id = ""
     user_id = ""
-    text = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
